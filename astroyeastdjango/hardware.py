86e85e32340edfdeb72c70fc83a9e6816af348ad
jbwaring <jean-baptiste@zetane.com>
2022-06-30 21:30:44 -0400

    hardware udpate

--- astroyeastdjango/hardware.py
+++ astroyeastdjango/hardware.py
@@ -2,20 +2,19 @@ from time import sleep
 import pigpio
 import subprocess
 
-pi = pigpio.pi()
+
 
 
 
 def RGB_test():
-	
+    print("***[hardware RGB_test]***")
+	pi = pigpio.pi()
 	brightness = 0
 	cycle_time = 0.01
 	pause_time = 2
 
 	subprocess.call(['sudo pigpiod'], shell=True)
-	
-	
-	while(brightness < 255):
+    while(brightness < 255):
 		brightness=brightness+1;
 		pi.set_PWM_dutycycle(18, brightness)
 		sleep(cycle_time)
@@ -50,4 +49,4 @@ def RGB_test():
 		sleep(cycle_time)
 		
 	pi.stop()	
-
+    return
